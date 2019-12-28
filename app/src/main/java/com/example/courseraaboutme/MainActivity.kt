4573748a4e1f2ae258a89083c022ba0aea638aa7
Szymon <szymon.nedzi@gmail.com>
2019-12-28 01:41:21 +0100

    text field with name, extract resources to appropriate files

--- app/src/main/java/com/example/courseraaboutme/MainActivity.kt
+++ app/src/main/java/com/example/courseraaboutme/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.activity_main)
     }
 }
